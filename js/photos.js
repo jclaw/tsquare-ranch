b1c72ba501aa8437229eb182f307eaf8fb3999d2
Jackson Clawson <jclaw3456@gmail.com>
2025-08-17 14:23:24 -0400

    add more images

--- js/photos.js
+++ js/photos.js
@@ -1,25 +1,25 @@
 const PHOTOS = [
   // spring
   {
-    filename: "IMG_6439.jpeg",
+    filename: "spring-1.jpeg",
     date: "2025-05-07"
   },
   {
-    filename: "IMG_6449.jpeg",
+    filename: "spring-2.jpeg",
     date: "2025-05-07"
   },
   {
-    filename: "IMG_6450.jpeg",
+    filename: "spring-3.jpeg",
     date: "2025-05-07"
   },
   {
-    filename: "IMG_6466.jpeg",
+    filename: "spring-4.jpeg",
     date: "2025-05-08"
   },
 
   // summer
   {
-    filename: "summer-1.JPG",
+    filename: "summer-1.jpeg",
     date: "2016-07-29"
   },
   {
@@ -29,6 +29,10 @@ const PHOTOS = [
   {
     filename: "summer-4.jpeg",
     date: "2022-07-08"
+  },
+  {
+    filename: "summer-5.jpeg",
+    date: "2025-08-06"
   },
 
   // fall
@@ -60,6 +64,22 @@ const PHOTOS = [
     filename: "fall-7.jpeg",
     date: "2022-10-10",
   },
+  {
+    filename: "fall-8.jpeg",
+    date: "2021-09-27",
+  },
+  {
+    filename: "fall-9.jpeg",
+    date: "2022-09-21",
+  },
+  {
+    filename: "fall-10.jpeg",
+    date: "2022-10-10",
+  },
+  {
+    filename: "fall-11.jpeg",
+    date: "2024-09-04",
+  },
   
   // winter
   {
@@ -89,5 +109,25 @@ const PHOTOS = [
   {
     filename: "winter-9.jpeg",
     date: "2023-01-02"
+  },
+  {
+    filename: "winter-10.jpeg",
+    date: "2023-01-21"
+  },
+  {
+    filename: "winter-11.jpeg",
+    date: "2020-12-28"
+  },
+  {
+    filename: "winter-12.jpeg",
+    date: "2023-03-10"
+  },
+  {
+    filename: "winter-13.jpeg",
+    date: "2024-03-16"
+  },
+  {
+    filename: "winter-14.jpeg",
+    date: "2024-03-20"
   }
 ]
